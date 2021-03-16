b98b63df0e9f3a29953fe40435394ea0533697e5
lockhaj1 <lockhartjared@gmail.com>
2021-03-16 17:08:50 -0400

    SuperUser View

---
+++ vu_speech/dashboard/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class Diagnostics(models.Model):
+    user_id = models.BigIntegerField('User ID')
+    speech_datetime = models.DateTimeField('Call Date and Time Initiated')
+    speech_duration = models.DurationField('Call Duration')
+    language = models.CharField('Language', max_length=50)
+    accent = models.CharField('Accent', max_length=50)
+    stability = models.DecimalField('Call Condifence', max_digits=2, decimal_places=2)
